7d7577bdc0ba3903c7386113f1b9f81b2da8e63d
miguelvilla <miguelvilla997@gmail.com>
2020-11-27 10:41:38 +0100

    Nuevos cambios

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -74,7 +74,7 @@ const EditProductScreen = (props) => {
 
   const submitHandler = useCallback(async () => {
     if (!formState.formIsValid) {
-      Alert.alert("Wrong input!", "Please check the errors in the form", [
+      Alert.alert("Wrong input!", "Please check the errors in the form!", [
         { text: "Ok" },
       ]);
       return;
